4ed31d0359e90160334c6285771fe79178674ed8
Scully87 <scully87.ts@googlemail.com>
2014-10-13 08:15:02 +0100

    data mapper setup added for heroku

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,6 +1,6 @@
 env = ENV["RACK_ENV"] || "development"
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/gritter_#{env}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://pbnsrezronvvkk:9DM3MVSNG38cf6Fbw4pl2Nbm8O@ec2-54-243-44-191.compute-1.amazonaws.com:5432/d4214lc09qdcfa")
 
 DataMapper.finalize
 
