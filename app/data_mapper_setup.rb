c05a55874f14598a75c15506817e9851c54aa05c
Scully87 <scully87.ts@googlemail.com>
2014-10-13 09:15:12 +0100

    procfile updated

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,7 +1,7 @@
 env = ENV["RACK_ENV"] || "development"
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://pbnsrezronvvkk:9DM3MVSNG38cf6Fbw4pl2Nbm8O@ec2-54-243-44-191.compute-1.amazonaws.com:5432/d4214lc09qdcfa")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/gritter_#{env}")
 
 DataMapper.finalize
 
-DataMapper.auto_migrate!
+DataMapper.auto_upgrade!
